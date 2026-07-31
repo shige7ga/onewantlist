f2f4f8f34970d9e1a9e81e293f99a634a100bd15
shige7ga <ameft_80@yahoo.co.jp>
2026-07-31 20:21:03 +0900

    ログイン後の遷移先を設定

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -24,6 +24,11 @@ class Users::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  # sign in後のredirect先を指定するメソッドを追加
+  def after_sign_in_path_for(resource)
+    root_path
+  end
 
   # sign out後のredirect先を指定するメソッドを追加
   def after_sign_out_path_for(resource_or_scope)
