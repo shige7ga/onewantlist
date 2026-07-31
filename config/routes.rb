9c868096b6d2219977ee4ff3e229c0032079edd5
shige7ga <ameft_80@yahoo.co.jp>
2026-07-31 19:11:47 +0900

    devise/usersコントローラー導入及びユーザー登録機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
+
   root "top#index"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
