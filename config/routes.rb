1ae31907a4dcf5301c04e755f9c1c86109dc5f84
shige7ga <ameft_80@yahoo.co.jp>
2026-08-02 14:54:31 +0900

    users#showのルーティング(マイページ用)を定義

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     sessions: "users/sessions"
   }
 
-
+  resources :users, only: [ :show ]
   root "top#index"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
