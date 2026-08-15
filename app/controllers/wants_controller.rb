8dc8f2b6095ee78112d3cd3628ee7f913f36b581
shige7ga <ameft_80@yahoo.co.jp>
2026-08-15 21:42:20 +0900

    やりたいこと登録機能実装及びヘッダーへ関連ボタン配置

--- app/controllers/wants_controller.rb
+++ app/controllers/wants_controller.rb
@@ -2,10 +2,25 @@ class WantsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @wants = [ "test", "test2" ]
+    @wants = current_user.wants
   end
 
   def new
     @want = current_user.wants.new
+  end
+
+  def create
+    @want = current_user.wants.new(want_params)
+    if @want.save
+      redirect_to wants_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def want_params
+    params.require(:want).permit(:content)
   end
 end
