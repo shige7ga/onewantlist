3f0998d03ca80f54d217d7ed50c9429e01d1fe1e
shige7ga <ameft_80@yahoo.co.jp>
2026-08-15 17:59:27 +0900

    ユーザーバリデーション追加・password文字数設定を変更・name空文字の場合の対応追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  # nameの空白を削除する or 空白文字等の場合にnilを返す
+  normalizes :name, with: ->(name) { name.strip.presence }
+  validates :name, length: { maximum: 40 }, allow_nil: true
 end
