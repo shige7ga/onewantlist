17151776e7eaa5d26a6099f8788c0315b105de7a
shige7ga <ameft_80@yahoo.co.jp>
2026-08-27 00:03:03 +0900

    contentの文字列前後に空白がある場合、空白削除する処理を実装

--- app/models/want.rb
+++ app/models/want.rb
@@ -3,6 +3,8 @@ class Want < ApplicationRecord
 
   validates :content, presence: true, length: { maximum: 400 }
 
+  normalizes :content, with: ->(value) { value.strip.presence }
+
   enum :status, {
     not_started: 0,
     in_progress: 1,
--- spec/models/want_spec.rb
+++ spec/models/want_spec.rb
@@ -8,5 +8,32 @@ RSpec.describe Want, type: :model do
         expect(want).to be_valid
       end
     end
+
+    context "contentについて" do
+      it "nilの場合、無効" do
+        want = build(:want, content: nil)
+        expect(want).to be_invalid
+      end
+
+      it "空文字の場合、無効" do
+        want = build(:want, content: "")
+        expect(want).to be_invalid
+      end
+
+      it "スペースのみの場合、無効" do
+        want = build(:want, content: " 　 ")
+        expect(want).to be_invalid
+      end
+
+      it "TABのみの場合、無効" do
+        want = build(:want, content: "\t\t")
+        expect(want).to be_invalid
+      end
+
+      it "contentの文字前後に空白の場合、前後空白が除去される" do
+        want = build(:want, content: "  Railsの勉強をする  ")
+        expect(want.content).to eq("Railsの勉強をする")
+      end
+    end
   end
 end
