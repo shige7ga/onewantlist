ec588862b92c6b141a5ef78a23c985ac845d1efc
shige7ga <ameft_80@yahoo.co.jp>
2026-08-26 20:56:29 +0900

    FactoryBotでWantテストデータ作成

---
+++ spec/factories/want.rb
@@ -0,0 +1,11 @@
+FactroyBot.define do
+  factory :want do
+    content { "富士山を登る" }
+    association :user
+
+    trait :with_details do
+      status { :in_progress }
+      due_date { 1.week.from_now }
+    end
+  end
+end
