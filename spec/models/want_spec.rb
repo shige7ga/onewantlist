431cb6ec96dbc7d1de574070bdb04417eef65c61
shige7ga <ameft_80@yahoo.co.jp>
2026-08-27 20:27:22 +0900

    wantのstatusのspecテスト追加

--- spec/models/want_spec.rb
+++ spec/models/want_spec.rb
@@ -2,14 +2,12 @@ require "rails_helper"
 
 RSpec.describe Want, type: :model do
   describe "バリデーション" do
-    context "正常系" do
-      it "やりたい内容が登録されていたら、やりたいこと登録できる" do
+    context "contentについて" do
+      it "やりたいことが登録されていれば、有効" do
         want = build(:want)
         expect(want).to be_valid
       end
-    end
 
-    context "contentについて" do
       it "nilの場合、無効" do
         want = build(:want, content: nil)
         expect(want).to be_invalid
@@ -33,6 +31,28 @@ RSpec.describe Want, type: :model do
       it "contentの文字前後に空白の場合、前後空白が除去される" do
         want = build(:want, content: "  Railsの勉強をする  ")
         expect(want.content).to eq("Railsの勉強をする")
+      end
+
+      it "400文字なら有効" do
+        want = build(:want, content: "a" * 400)
+        expect(want).to be_valid
+      end
+
+      it "401文字なら有効" do
+        want = build(:want, content: "a" * 401)
+        expect(want).to be_invalid
+      end
+    end
+
+    context "status(enum)について" do
+      it "デフォルトでnot_startedになっている" do
+        want = build(:want)
+        expect(want.status).to eq("not_started")
+      end
+
+      it "定義されたstatusを設定できる" do
+        want = build(:want, status: :in_progress)
+        expect(want.status).to eq("in_progress")
       end
     end
   end
