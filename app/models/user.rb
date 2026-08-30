de9936c2cae453c5429e0a01c27da5c84c7e63fe
shige7ga <ameft_80@yahoo.co.jp>
2026-08-30 18:53:01 +0900

    user_statusesテーブル作成及びユーザーとのリレーション定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :wants, dependent: :destroy
+  has_many :user_statuses, dependent: :destroy
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
