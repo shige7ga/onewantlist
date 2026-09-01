c8fdbfffd548e10b5d661d78992ad6729a74ee1c
shige7ga <ameft_80@yahoo.co.jp>
2026-09-01 22:39:03 +0900

    user_statusのバリデーション追加及びModelスペック実装

--- app/models/user_status.rb
+++ app/models/user_status.rb
@@ -11,4 +11,6 @@ class UserStatus < ApplicationRecord
   validates :longest_action_streak, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :last_login_date, presence: true, comparison: { less_than_or_equal_to: Date.current }
   validates :last_action_date, comparison: { less_than_or_equal_to: Date.current }, allow_nil: true
+  validates :random_gacha_count, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :random_gacha_date, comparison: { less_than_or_equal_to: Date.current }, allow_nil: true
 end
