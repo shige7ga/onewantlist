112e5123fc757b2d58e636b921bd88f9371109ff
shige7ga <ameft_80@yahoo.co.jp>
2026-09-04 23:27:58 +0900

    migrationファイルのprivateメソッドを無くす仕様に変更

--- db/migrate/20260901110154_rename_want_count_to_action_count_in_user_status.rb
+++ db/migrate/20260901110154_rename_want_count_to_action_count_in_user_status.rb
@@ -1,6 +1,5 @@
 class RenameWantCountToActionCountInUserStatus < ActiveRecord::Migration[8.1]
   def up
-    # 過去のマイグレーションを後から変更したことで、本番DBに存在しないカラムを補完する
     unless column_exists?(:user_statuses, :login_count)
       add_column :user_statuses,
                  :login_count,
@@ -9,18 +8,56 @@ class RenameWantCountToActionCountInUserStatus < ActiveRecord::Migration[8.1]
                  null: false
     end
 
-    # 本番DBでは experiment、ローカルDBでは experimence の可能性がある
     if column_exists?(:user_statuses, :experiment) &&
        !column_exists?(:user_statuses, :experimence)
       rename_column :user_statuses, :experiment, :experimence
     end
 
-    rename_or_add_action_column
-    rename_action_streak_columns
-    rename_last_action_date_column
-    add_gacha_columns
+    if column_exists?(:user_statuses, :want_registration_count)
+      rename_column :user_statuses,
+                    :want_registration_count,
+                    :action_count
+    elsif !column_exists?(:user_statuses, :action_count)
+      add_column :user_statuses,
+                 :action_count,
+                 :integer,
+                 default: 0,
+                 null: false
+    end
 
-    # 既存データを現在のバリデーションに合わせる
+    if column_exists?(:user_statuses, :want_registration_streak) &&
+       !column_exists?(:user_statuses, :action_streak)
+      rename_column :user_statuses,
+                    :want_registration_streak,
+                    :action_streak
+    end
+
+    if column_exists?(:user_statuses, :longest_want_registration_streak) &&
+       !column_exists?(:user_statuses, :longest_action_streak)
+      rename_column :user_statuses,
+                    :longest_want_registration_streak,
+                    :longest_action_streak
+    end
+
+    if column_exists?(:user_statuses, :last_registration_date) &&
+       !column_exists?(:user_statuses, :last_action_date)
+      rename_column :user_statuses,
+                    :last_registration_date,
+                    :last_action_date
+    end
+
+    unless column_exists?(:user_statuses, :random_gacha_count)
+      add_column :user_statuses,
+                 :random_gacha_count,
+                 :integer,
+                 default: 0,
+                 null: false
+    end
+
+    unless column_exists?(:user_statuses, :random_gacha_date)
+      add_column :user_statuses, :random_gacha_date, :date
+    end
+
     execute <<~SQL.squish
       UPDATE user_statuses
       SET login_streak = 1
@@ -46,60 +83,5 @@ class RenameWantCountToActionCountInUserStatus < ActiveRecord::Migration[8.1]
 
   def down
     raise ActiveRecord::IrreversibleMigration
-  end
-
-  private
-
-  def rename_or_add_action_column
-    if column_exists?(:user_statuses, :want_registration_count)
-      rename_column :user_statuses,
-                    :want_registration_count,
-                    :action_count
-    elsif !column_exists?(:user_statuses, :action_count)
-      add_column :user_statuses,
-                 :action_count,
-                 :integer,
-                 default: 0,
-                 null: false
-    end
-  end
-
-  def rename_action_streak_columns
-    if column_exists?(:user_statuses, :want_registration_streak) &&
-       !column_exists?(:user_statuses, :action_streak)
-      rename_column :user_statuses,
-                    :want_registration_streak,
-                    :action_streak
-    end
-
-    if column_exists?(:user_statuses, :longest_want_registration_streak) &&
-       !column_exists?(:user_statuses, :longest_action_streak)
-      rename_column :user_statuses,
-                    :longest_want_registration_streak,
-                    :longest_action_streak
-    end
-  end
-
-  def rename_last_action_date_column
-    if column_exists?(:user_statuses, :last_registration_date) &&
-       !column_exists?(:user_statuses, :last_action_date)
-      rename_column :user_statuses,
-                    :last_registration_date,
-                    :last_action_date
-    end
-  end
-
-  def add_gacha_columns
-    unless column_exists?(:user_statuses, :random_gacha_count)
-      add_column :user_statuses,
-                 :random_gacha_count,
-                 :integer,
-                 default: 0,
-                 null: false
-    end
-
-    unless column_exists?(:user_statuses, :random_gacha_date)
-      add_column :user_statuses, :random_gacha_date, :date
-    end
   end
 end
