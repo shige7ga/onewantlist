dc4d80e9d451b287c42b35438d23cb31eb5ecffc
shige7ga <ameft_80@yahoo.co.jp>
2026-09-13 22:22:15 +0900

    debugツール導入

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,8 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  BetterErrors::Middleware.allow_ip! "0.0.0.0/0"
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Make code changes take effect immediately without server restart.
