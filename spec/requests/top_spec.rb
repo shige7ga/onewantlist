52cff3d16842a3614f5372eb6d28cd043983af4f
shige7ga <ameft_80@yahoo.co.jp>
2026-09-15 23:08:55 +0900

    他コントローラにRequestスペック実装

---
+++ spec/requests/top_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "Top", type: :request do
+  let(:user) { create(:user) }
+
+  describe "GET /" do
+    context "ログイン時" do
+      before do
+        sign_in user
+      end
+      it "マイページにリダイレクトされる" do
+        get root_path
+        expect(response).to redirect_to(mypage_path)
+      end
+    end
+
+    context "未ログイン時" do
+      it "正常にレスポンスが返る" do
+        get root_path
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
+end
---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :request do
+  describe "GET /mypage" do
+    let(:user) { create(:user) }
+
+    context "ログイン時" do
+      before do
+        sign_in user
+      end
+
+      let!(:my_want) { create(:want, user: user, content: "映画を見る") }
+      let!(:other_user) { create(:user, email: "other@test") }
+      let!(:other_want) { create(:want, user: other_user,content: "富士山に登る") }
+
+      it "正常にレスポンスが返る" do
+        get mypage_path
+        expect(response).to have_http_status(200)
+      end
+
+      it "自分のやりたいことだけ表示される" do
+        get mypage_path
+        expect(response.body).to include("映画を見る")
+        expect(response.body).not_to include("富士山に登る")
+      end
+    end
+
+    context "未ログイン時" do
+      it "ログイン画面にリダイレクトされる" do
+        get mypage_path
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+end
