81c5d9744eec05e57be493af513834f4eb853e75
shige7ga <ameft_80@yahoo.co.jp>
2026-09-16 19:23:18 +0900

    トップページのsystem spec実装

--- spec/system/top_spec.rb
+++ spec/system/top_spec.rb
@@ -1,9 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe "Top", type: :system do
-  it "トップページを表示できる" do
-    visit root_path
+  context "未ログイン時" do
+    it "トップページが表示される" do
+      visit root_path
 
-    expect(page).to have_css("body")
+      expect(page).to have_content("わんわんとリスト")
+      expect(page).to have_content("1日1つだけ「やりたいこと」を登録して")
+      expect(page).to have_link("ユーザー登録")
+      expect(page).to have_link("ログイン")
+    end
+
+    it "ログイン画面へ移動できる" do
+      visit root_path
+      within(".hero") do
+        click_link "ログイン"
+      end
+
+      expect(page).to have_current_path(new_user_session_path)
+    end
+
+    it "ユーザー登録画面へ移動できる" do
+      visit root_path
+      within(".hero") do
+        click_link "ユーザー登録"
+      end
+
+      expect(page).to have_current_path(new_user_registration_path)
+    end
+  end
+
+  context "ログイン時" do
+    let!(:user) { create(:user) }
+
+     before do
+      visit new_user_session_path
+
+      fill_in "メールアドレス", with: user.email
+      fill_in "パスワード", with: user.password
+      click_button "ログイン"
+
+      expect(page).to have_current_path(mypage_path)
+    end
+
+    it "トップページではなくマイページが表示される" do
+      visit root_path
+      expect(page).to have_current_path(mypage_path)
+    end
   end
 end
