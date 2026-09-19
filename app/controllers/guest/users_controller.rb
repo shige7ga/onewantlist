43b5f2ac6dc48efef53403795e67e12bd1a9ee1a
shige7ga <ameft_80@yahoo.co.jp>
2026-09-19 19:55:08 +0900

    ゲストページを仮実装

--- app/controllers/guest/users_controller.rb
+++ app/controllers/guest/users_controller.rb
@@ -1,4 +1,8 @@
 class Guest::UsersController < ApplicationController
   def show
+    # 一時的にデータを格納
+    @user = User.new(name: "ゲストユーザー")
+    @user_status = UserStatus.new(user: @user)
+    @wants = Want.none
   end
 end
